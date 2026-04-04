bbe48f65ba28a8314f225e80c0d062f02015a11f
jihwan38 <sss203800@gmail.com>
2026-04-04 22:51:27 +0900

    feat: 인터프리터 로직 구현
    - Interp.hs: value_of 함수를 통해 AST(Let, If, Var 등)의 패턴 매칭 및 평가 로직 완성
    - initEnv 기초 환경 구성 및 전체 26개 평가 테스트 케이스(Spec) 통과 확인

--- ch3/app/letlang/Interp.hs
+++ ch3/app/letlang/Interp.hs
@@ -8,19 +8,45 @@ import Env
 --
 value_of :: Exp -> Env -> ExpVal
 
-value_of exp env = 
-  error "TODO: implement a value_of function"
-    
+value_of (Const_Exp n) env = Num_Val n 
+
+value_of (Diff_Exp exp1 exp2) env = 
+    let val1 = value_of exp1 env
+        val2 = value_of exp2 env
+    in Num_Val (expval_num val1 - expval_num val2)
+
+value_of (Var_Exp s) env = 
+    apply_env env s
+
+value_of (IsZero_Exp exp) env =
+    let val1 = value_of exp env
+        num1 = expval_num val1
+    in if num1 == 0 then Bool_Val True
+       else Bool_Val False
+
+value_of (If_Exp exp1 exp2 exp3) env = 
+    let val1 = value_of exp1 env
+        bool1 = expval_bool val1
+    in if bool1 then value_of exp2 env
+       else value_of exp3 env
+
+value_of (Let_Exp var exp1 body) env = 
+    let val1 = value_of exp1 env
+        new_env = extend_env var val1 env
+    in value_of body new_env
 
 --
 value_of_program :: Exp -> ExpVal
 
-value_of_program exp = error "TODO: implement a value_of_program function"
+value_of_program exp = value_of exp initEnv
 
 -- [TODO] Complete initEnv.
 --   { x |-> 10, v |-> 5, i |-> 1 }
 initEnv :: Env
-initEnv = undefined
+initEnv = extend_env "i" (Num_Val 1)
+            (extend_env "v" (Num_Val 5)
+              (extend_env "x" (Num_Val 10)
+                empty_env))
 
           
 
