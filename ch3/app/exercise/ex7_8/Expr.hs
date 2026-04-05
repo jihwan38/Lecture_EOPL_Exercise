33ca317cc42aaf7b441986d823d51598d498a0b4
jihwan38 <sss203800@gmail.com>
2026-04-06 00:22:16 +0900

    feat: Expr.hs 수정

--- ch3/app/exercise/ex7_8/Expr.hs
+++ ch3/app/exercise/ex7_8/Expr.hs
@@ -10,7 +10,13 @@ type Program = Exp
 data Exp = 
     Const_Exp Int
   | Diff_Exp Exp Exp
+  | Add_Exp Exp Exp
+  | Mul_Exp Exp Exp
+  | Quot_Exp Exp Exp
   | IsZero_Exp Exp
+  | IsEqual_Exp Exp Exp
+  | IsGreater_Exp Exp Exp
+  | IsLess_Exp Exp Exp
   | If_Exp Exp Exp Exp
   | Var_Exp Identifier
   | Let_Exp Identifier Exp Exp
@@ -20,8 +26,14 @@ type Identifier = String
 
 const_exp n = Const_Exp n
 diff_exp e1 e2 = Diff_Exp e1 e2
+add_exp e1 e2  = Add_Exp e1 e2
+mul_exp e1 e2  = Mul_Exp e1 e2
+quot_exp e1 e2 = Quot_Exp e1 e2
 
-iszero_exp e = IsZero_Exp e
+iszero_exp e   = IsZero_Exp e
+isequal_exp e1 e2   = IsEqual_Exp e1 e2
+isgreater_exp e1 e2 = IsGreater_Exp e1 e2
+isless_exp e1 e2    = IsLess_Exp e1 e2
 
 if_exp e1 e2 e3 = If_Exp e1 e2 e3
 
